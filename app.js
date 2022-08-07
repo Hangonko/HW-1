1912ac5c3239192cec8f6fffda0dd7e4b61e8159
Saba637 <93818290+Saba637@users.noreply.github.com>
2022-08-08 02:27:55 +0400

    .

---
+++ app.js
@@ -0,0 +1,36 @@
+// --------------- დავალება 1 --------------- //
+
+// const arr = [1,-2,4,8,-7,10];
+
+// const newArr = arr.filter((x) => x > 0);
+
+// const sumOfPositive = newArr.reduce((total, current) => {
+//     return total+current;
+// }, 0);
+
+// console.log(sumOfPositive);
+
+// --------------- დავალება 2 --------------- //
+
+// const stringArr = ["a", "b", "a", "c", "b", "a"];
+// var a = 0;
+// var b = 0;
+// var c = 0;
+
+// const calculate = stringArr.forEach((item) => {
+//   if (item === "a") {
+//     return a++;
+//   }
+//   if (item === "b") {
+//     return b++;
+//   }
+//   if (item === "c") {
+//     return c++;
+//   }
+// });
+
+// const calculated = { a: a, b: b, c: c };
+
+// console.log(calculated);
+
+// --------------- დავალება 3 --------------- //
